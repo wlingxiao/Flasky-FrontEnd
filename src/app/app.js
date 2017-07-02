e1c413f003fe2ec838ebcbb96958f05a3587661b
wlingxiao <wlingxiao@outlook.com>
2017-07-02 17:44:06 +0800

    add index page

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,22 @@
 import angular from 'angular';
 import '@uirouter/angularjs'
 
+import indexDirective from './index/index.directive'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = angular.module('app', ['ui.router']);
 
+app.directive('index', indexDirective);
+
 app.config(function ($stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
+
+    const indexState = {
+        name: 'index',
+        url: '/',
+        template: '<index></index>'
+    };
+
+    $stateProvider.state(indexState);
 });
---
+++ src/app/index/index.directive.js
@@ -0,0 +1,30 @@
+import indexTpl from './index.tpl.html'
+
+export default function () {
+    return {
+        restrict: 'E',
+        template: indexTpl,
+        scope: true,
+        controller: function ($scope, $http) {
+            $http.get('/post/load_all_posts').then(function (response) {
+                const data = response['data'];
+                if (data && data['code'] === 200) {
+                    const posts = [];
+                    const postsData = data['data'];
+                    for (let i = 0; i < postsData.length; i++) {
+                        posts.push({
+                            id: postsData[i]['id'],
+                            title: postsData[i]['title'],
+                            content: postsData[i]['content'],
+                            createTime: postsData[i]['create_time']
+                        })
+                    }
+                    $scope.posts = posts;
+                }
+
+            })['catch'](function (error) {
+
+            })
+        }
+    }
+}
