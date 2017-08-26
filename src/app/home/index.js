87451c17689fde03e9a11169b55852044f833da4
wlingxiao <wlingxiao@outlook.com>
2017-08-26 15:24:25 +0800

    完善首页样式

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -3,6 +3,8 @@ import augular from 'angular'
 import homeDirective from './home.directive'
 import postsService from './posts.service'
 
+import './home.css'
+
 export default augular.module('home', [])
     .directive('home', homeDirective)
     .service('postsService', postsService);
